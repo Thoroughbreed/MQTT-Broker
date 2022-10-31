f91c39276dd3c64357126ff1571ddf6adb295750
Jan Andreasen <jan@tved.it>
2022-10-31 13:10:40 +0100

    Username/pwd works

---
+++ MQTTBroker/Config.cs
@@ -0,0 +1,14 @@
+namespace MQTTBroker;
+
+public class Config
+{
+    /// <summary>
+    ///     Gets or sets the port.
+    /// </summary>
+    public int Port { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the list of valid users.
+    /// </summary>
+    public List<User> Users { get; set; } = new();
+}
--- MQTTBroker/Program.cs
+++ MQTTBroker/Program.cs
@@ -1,6 +1,9 @@
-﻿using MQTTnet.Server;
+﻿using System.Reflection;
+using MQTTnet.Server;
 using MQTTnet;
 using System.Text;
+using MQTTnet.Protocol;
+using Newtonsoft.Json;
 using static System.Console;
 
 // See https://aka.ms/new-console-template for more information
@@ -8,40 +11,175 @@ namespace MQTTBroker
 {
     internal class Program
     {
+        private static readonly List<string> ClientIdPrefixesUsed = new();
+        private static Config config = new();
+
         static async Task Main(string[] args)
         {
-            // Create the options for MQTT Broker
+            var currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty;
+            config = ReadConfiguration(currentPath);
             var option = new MqttServerOptionsBuilder()
-                //Set endpoint to localhost
-                .WithDefaultEndpoint()
-                //Add Interceptor for logging incoming messages
-                .WithApplicationMessageInterceptor(OnNewMessage);
+                .WithDefaultEndpoint();
 
             // Create a new mqtt server 
-            var mqttServer = new MqttFactory().CreateMqttServer();
-            await mqttServer.StartAsync(option.Build());
-            
+            var server = new MqttFactory().CreateMqttServer(option.Build());
+            server.InterceptingPublishAsync += Server_InterceptingPublishAsync;
+            server.ValidatingConnectionAsync += ValidateConnectionAsync;
+            await server.StartAsync();
+
             // Keep application running until user press a key
             WriteLine("Press ENTER to quit.");
             ReadLine();
         }
 
-        static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        private static Task ValidateConnectionAsync(ValidatingConnectionEventArgs args)
         {
+            try
+            {
+                var currentUser = config.Users.FirstOrDefault(u => u.UserName == args.UserName);
+
+                if (currentUser == null)
+                {
+                    args.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                    WriteLine("currentUser == Null");
+                    // LogMessage(args, true);
+                    return Task.CompletedTask;
+                }
+
+                if (args.UserName != currentUser.UserName)
+                {
+                    args.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                    WriteLine("UserName != UserName");
+                    // LogMessage(args, true);
+                    return Task.CompletedTask;
+                }
+
+                if (args.Password != currentUser.Password)
+                {
+                    args.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                    WriteLine("Password != Password");
+                    // LogMessage(args, true);
+                    return Task.CompletedTask;
+                }
+
+                if (!currentUser.ValidateClientId)
+                {
+                    args.ReasonCode = MqttConnectReasonCode.Success;
+                    args.SessionItems.Add(args.ClientId, currentUser);
+                    WriteLine("!ValidateClientID");
+                    WriteLine(currentUser.ClientId);
+                    WriteLine(currentUser.ClientIdPrefix);
+                    // LogMessage(args, false);
+                    return Task.CompletedTask;
+                }
+
+                if (string.IsNullOrWhiteSpace(currentUser.ClientIdPrefix))
+                {
+                    if (args.ClientId != currentUser.ClientId)
+                    {
+                        args.ReasonCode = MqttConnectReasonCode.BadUserNameOrPassword;
+                        WriteLine("Bad username or pwd");
+                        // LogMessage(args, true);
+                        return Task.CompletedTask;
+                    }
+
+                    args.SessionItems.Add(currentUser.ClientId, currentUser);
+                }
+                else
+                {
+                    if (!ClientIdPrefixesUsed.Contains(currentUser.ClientIdPrefix))
+                    {
+                        ClientIdPrefixesUsed.Add(currentUser.ClientIdPrefix);
+                    }
+
+                    args.SessionItems.Add(currentUser.ClientIdPrefix, currentUser);
+                }
+
+                args.ReasonCode = MqttConnectReasonCode.Success;
+                WriteLine("yay!");
+                // LogMessage(args, false);
+                return Task.CompletedTask;
+            }
+            catch (Exception ex)
+            {
+                // Logger.Error("An error occurred: {Exception}.", ex);
+                return Task.FromException(ex);
+            }
+        }
+
+        static Task Server_InterceptingPublishAsync(InterceptingPublishEventArgs args)
+        {
+            WriteLine(args.ClientId);
+            var clientIdPrefix = GetClientIdPrefix(args.ClientId);
+            User? currentUser = null;
+
+            if (string.IsNullOrWhiteSpace(clientIdPrefix))
+            {
+                if (args.SessionItems.Contains(args.ClientId))
+                {
+                    currentUser = args.SessionItems[args.ClientId] as User;
+                    WriteLine(currentUser.ClientId);
+                    WriteLine(currentUser.ClientIdPrefix);
+                }
+            }
+            else
+            {
+                if (args.SessionItems.Contains(clientIdPrefix))
+                {
+                    currentUser = args.SessionItems[clientIdPrefix] as User;
+                    WriteLine(currentUser.ClientId);
+                    WriteLine(currentUser.ClientIdPrefix);
+                }
+            }
+            
             // Convert Payload to string
-            var payload = context.ApplicationMessage?.Payload == null
+            var payload = args.ApplicationMessage?.Payload == null
                 ? null
-                : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+                : Encoding.UTF8.GetString(args.ApplicationMessage?.Payload);
 
 
             WriteLine(
                 " TimeStamp: {0} -- Message: ClientId = {1}, Topic = {2}, Payload = {3}, QoS = {4}, Retain-Flag = {5}",
                 DateTime.Now,
-                context.ClientId,
-                context.ApplicationMessage?.Topic,
+                args.ClientId,
+                args.ApplicationMessage?.Topic,
                 payload,
-                context.ApplicationMessage?.QualityOfServiceLevel,
-                context.ApplicationMessage?.Retain);
+                args.ApplicationMessage?.QualityOfServiceLevel,
+                args.ApplicationMessage?.Retain);
+            return Task.CompletedTask;
+        }
+
+        private static string GetClientIdPrefix(string clientId)
+        {
+            foreach (var clientIdPrefix in ClientIdPrefixesUsed)
+            {
+                if (clientId.StartsWith(clientIdPrefix))
+                {
+                    return clientIdPrefix;
+                }
+            }
+
+            return string.Empty;
+        }
+
+        private static Config ReadConfiguration(string currentPath)
+        {
+            var filePath = $"{currentPath}/config.json";
+
+            if (File.Exists(filePath))
+            {
+                Config config;
+                using (var r = new StreamReader(filePath))
+                {
+                    var json = r.ReadToEnd();
+                    config = JsonConvert.DeserializeObject<Config>(json) ?? new();
+                }
+
+
+                return config;
+            }
+
+            return new Config();
         }
     }
 }
